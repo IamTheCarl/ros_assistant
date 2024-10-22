7c10ddaa57970b08ee6d4991f4904967cf89d58d
James Carl <jamescarl96@gmail.com>
2024-10-21 23:55:45 -0400

    Print which ros tunnel a log message is coming from

--- packages/rass-cli/src/tunnel.rs
+++ packages/rass-cli/src/tunnel.rs
@@ -1,10 +1,34 @@
 use std::{
-    io,
-    process::{Command, Stdio},
+    io::{self, BufRead, BufReader},
+    process::{ChildStderr, Command, Stdio},
 };
 
 use crate::{arguments::TunnelCommand, host_config::HostConfig, load_project};
 use anyhow::{bail, Context, Result};
+
+fn log_wrapper(prefix: &str, input: ChildStderr) {
+    // We need the buffer for that line reading.
+    let mut input = BufReader::new(input);
+    let mut buffer = String::from(prefix);
+
+    loop {
+        buffer.clear();
+
+        match input.read_line(&mut buffer) {
+            Ok(0) => break, // Indicates end of file.
+            Ok(chars_read) => {
+                // Normal input.
+                // Truncate the newline off.
+                buffer.truncate(chars_read - 1);
+                log::info!("{prefix}: {}", buffer);
+            }
+            Err(error) => {
+                log::error!("Error piping stderr from `{prefix}`: {error:?}");
+                break;
+            }
+        }
+    }
+}
 
 pub(crate) fn tunnel(args: TunnelCommand) -> Result<()> {
     let (project_root, ssh_config) = load_project(args.project_root)?;
@@ -35,25 +59,27 @@ pub(crate) fn tunnel(args: TunnelCommand) -> Result<()> {
 
     ssh_command
         .stdout(Stdio::piped())
-        .stderr(Stdio::inherit())
+        .stderr(Stdio::piped())
         .stdin(Stdio::piped());
 
     let mut ssh = ssh_command.spawn().context("Failed to spawn ssh session")?;
     let mut ssh_in = ssh.stdin.take().unwrap();
     let mut ssh_out = ssh.stdout.take().unwrap();
+    let ssh_stderr = ssh.stderr.take().unwrap();
 
     // TODO I want to rename that package.
     let mut tunnel_command = Command::new("dds_bridge");
     tunnel_command
         .stdout(Stdio::piped())
         .stdin(Stdio::piped())
-        .stderr(Stdio::inherit());
+        .stderr(Stdio::piped());
 
     let mut tunnel = tunnel_command
         .spawn()
         .context("Failed to spawn ROS Tunnel")?;
     let mut tunnel_in = tunnel.stdin.take().unwrap();
     let mut tunnel_out = tunnel.stdout.take().unwrap();
+    let tunnel_stderr = tunnel.stderr.take().unwrap();
 
     // Spawn some threads to copy the standard inputs and output between the two processes.
     let ssh_to_tunnel_thread = std::thread::spawn(move || {
@@ -67,6 +93,14 @@ pub(crate) fn tunnel(args: TunnelCommand) -> Result<()> {
         }
     });
 
+    // Spawn some threads to forward logs and label which tunnel instance the output is coming from.
+    let local_tunnel_log_forwarding = std::thread::spawn(move || {
+        log_wrapper("local", tunnel_stderr);
+    });
+    let remote_tunnel_log_forwarding = std::thread::spawn(move || {
+        log_wrapper("remote", ssh_stderr);
+    });
+
     let tunnel_result = tunnel
         .wait()
         .context("Failed to wait for tunnel command.")?;
@@ -78,6 +112,14 @@ pub(crate) fn tunnel(args: TunnelCommand) -> Result<()> {
 
     if let Err(error) = ssh_to_tunnel_thread.join() {
         log::error!("Ssh to tunnel thread panicked: {error:?}");
+    }
+
+    if let Err(error) = local_tunnel_log_forwarding.join() {
+        log::error!("Local tunnel log forwarding panicked: {error:?}");
+    }
+
+    if let Err(error) = remote_tunnel_log_forwarding.join() {
+        log::error!("Remote tunnel log forwarding panicked: {error:?}");
     }
 
     if ssh_result.success() && tunnel_result.success() {
