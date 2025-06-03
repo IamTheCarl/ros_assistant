6aba3a9e85cb680f3abfcf9a89b38ddd907f1ef8
James Carl <jamescarl96@gmail.com>
2025-06-03 10:46:59 -0400

    Cleaner cross compilation

--- packages/firmware/example_projects/demo_setup/firmware/default.nix
+++ packages/firmware/example_projects/demo_setup/firmware/default.nix
@@ -1,33 +1,30 @@
 { pkgs ? import <nixpkgs> { } }:
 let
-  rust = pkgs.callPackage ./nix-libs/rust.nix {};
-  rust_platform = pkgs.makeRustPlatform {
+  pkgs_cross = import pkgs.path {
+    config = {
+      allowUnsupportedSystem = true;
+    };
+    crossSystem = pkgs.lib.systems.examples.arm-embedded // {
+      rustc = {
+        config = "thumbv7em-none-eabihf";
+      };
+    };
+  };
+  rust = pkgs_cross.callPackage ./nix-libs/rust.nix {};
+  rust_platform = pkgs_cross.makeRustPlatform {
     cargo = rust;
     rustc = rust;
   };
-
-  common_dependencies = pkgs.callPackage ./nix-libs/build_deps.nix {};
 in
 rust_platform.buildRustPackage rec {
   pname = "embedded_firmware_demo";
   version = "0.0.1";
  
-  nativeBuildInputs = common_dependencies;
+  RUSTFLAGS = [
+    "-C"
+    "linker=${pkgs_cross.stdenv.cc.targetPrefix}ld"
+  ];
 
   src = ./.;
-  cargoHash = "sha256-W02262jLC9KIUUyHKMu2LZE4DMawEoO4gUBYKUJjatE=";
-
-  buildPhase = ''
-    # This hack is needed to work around the fact that Nix is *very incistent* on using
-    # Cargo-Auditable. It's incompatible with the rust-lld linker we need forthe ARM target.
-    # We also have to pass the thumbv7em target ourselves because I haven't been able to
-    # successfully bring up a proper Nix cross compiler platform for that target and we
-    # really don't need the whole cross compiler target for this paticular use case.
-    ${rust}/bin/cargo build --target thumbv7em-none-eabihf --offline --release
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp target/thumbv7em-none-eabihf/release/dfu_demo $out/bin/
-  '';
+  cargoHash = "sha256-LntGAlek3O49wNtXuZMfDA2StJohjs75SHX/X5LYRLU=";
 }
