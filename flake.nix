5643d1d71842ca07c690afba8c335839240fc805
James Carl <jamescarl96@gmail.com>
2025-08-23 03:38:25 -0400

    Create root flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rass-cli = {
+      url = "./packages/rass-cli";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+	rust-overlay.follows = "rust-overlay";
+      };
+    };
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rass-cli, flake-utils, ... }: {
+    rosify = ./packages/rosify_package;
+  } // flake-utils.lib.eachDefaultSystem (system:
+  {
+    packages = {
+      rass-cli = rass-cli.packages.${system}.default;
+    };
+  });
+}
