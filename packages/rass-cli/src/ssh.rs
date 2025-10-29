797f871fb613d4c8061406f92c7ed5a1cd387d84
James Carl <jamescarl96@gmail.com>
2025-10-29 02:37:11 -0400

    Convert to async

--- packages/rass-cli/src/ssh.rs
+++ packages/rass-cli/src/ssh.rs
@@ -2,8 +2,9 @@ use anyhow::{Context, Result};
 
 use crate::{arguments, ProjectContext};
 
-pub fn ssh(args: arguments::SshCommand) -> Result<()> {
+pub async fn ssh(args: arguments::SshCommand) -> Result<()> {
     let context = ProjectContext::load_project(vec![], args.project_root, None, None)
+        .await
         .context("Failed to initalize build")?;
 
     let host = if let Some(host) = args.host {
@@ -11,8 +12,11 @@ pub fn ssh(args: arguments::SshCommand) -> Result<()> {
     } else {
         context
             .select_default_host()
+            .await
             .context("Failed to select default host for robot")?
     };
 
-    context.run_ssh(host.as_str(), args.command.as_ref().map(|c| c.as_str()))
+    context
+        .run_ssh(host.as_str(), args.command.as_ref().map(|c| c.as_str()))
+        .await
 }
